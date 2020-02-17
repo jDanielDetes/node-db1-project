1acaaebeaa9ac5b62887fafa1d0a606f48db727a
jDanielDetes <jdanieldetes@gmail.com>
2020-02-17 17:59:19 -0500

    account ID up

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -13,8 +13,26 @@ router.get("/", (req,res)=>{
     })
     .catch(error =>{
         console.log(error)
-        res.status(500).json({error:"failed to get the list of post"})
+        res.status(500).json({error:"failed to get the list of accounts"})
     })
 })
 
+router.get("/:id",(req,res)=>{
+getById(req.params.id)
+.then(account =>{
+    res.status(200).json(account)
+})
+.catch(error =>{
+    console.log(error)
+    res.status(500).json({error:"failed to get the account"})
+})
+})
+
 module.exports = router;
+
+function getById(id) {
+    return db("accounts")
+      .where({ id })
+      .first();
+  }
+  
