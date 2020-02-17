8b8b0299f989bfedc31fd8d49005750389478507
jDanielDetes <jdanieldetes@gmail.com>
2020-02-17 17:45:52 -0500

    accounts set up

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const accountRouter= require('./accounts/account-router.js')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountRouter)
+
+
+
 
 module.exports = server;
